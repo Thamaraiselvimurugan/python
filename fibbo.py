ca412f7eb3008582f4e49c1d14c05fffcaafbf2d
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-07-03 21:54:15 +0530

    Add files via upload

---
+++ fibbo.py
@@ -0,0 +1,30 @@
+print("Enter the number")
+
+a=input()
+
+def fibonacci(a):
+
+  s=[]
+
+  i=0
+
+  a1=0
+
+  a2=1
+
+  s.append(a1)
+
+  s.append(a2)
+
+  while i<a:
+
+    (a1,a2)=(a2,a1+a2)
+
+    s.append(a2)
+
+    i=i+1 
+
+  print(s)
+
+fibonacci(a)
+
